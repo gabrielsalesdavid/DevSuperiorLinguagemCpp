2ee99b02d422a8a01014e7f2617320cdda9c1acb
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-08 20:54:53 -0300

    Saida de dados em C++...

---
+++ SaidaDeDados.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int x, y;
+
+    x = 20;
+    y = 10;
+
+    cout << "O valor de X é: " << x << endl;
+    cout << "O valor de Y é: " << y << endl;
+
+    return 0;
+}
